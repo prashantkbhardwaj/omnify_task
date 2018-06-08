700412f700eecf93fc1c79fe2a1cc859ed69023d
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-06-08 23:44:35 +0530

    fix from loacal

--- application/views/success.php
+++ application/views/success.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>success</title>
-</head>
-<body>
-<h1>Success</h1>
-</body>
-</html>
+<?php
+if (isset($_GET['code'])) {
+  $client->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client->getAccessToken();
+  $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+  echo $redirect; die();
+//  header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
+}
+?>
