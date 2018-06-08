91e2ad4becfd1ef4cf7a04b2b866b3413fdf16de
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-06-08 21:47:02 +0530

    adding fix

--- index.php
+++ index.php
@@ -1,45 +1,121 @@
 <?php
-/*  GOOGLE LOGIN BASIC - Tutorial
- *  file            - index.php
- *  Developer       - Krishna Teja G S
- *  Website         - http://packetcode.com/apps/google-login/
- *  Date            - 28th Aug 2015
- *  license         - GNU General Public License version 2 or later
-*/
 
-// REQUIREMENTS - PHP v5.3 or later
-// Note: The PHP client library requires that PHP has curl extensions configured. 
-
-/*
- * DEFINITIONS
+/**
+ * The directory in which your application specific resources are located.
+ * The application directory must contain the bootstrap.php file.
  *
- * load the autoload file
- * define the constants client id,secret and redirect url
- * start the session
+ * @link http://kohanaframework.org/guide/about.install#application
  */
-require_once __DIR__.'/gplus-lib/vendor/autoload.php';
+$application = 'application';
 
+/**
+ * The directory in which your modules are located.
+ *
+ * @link http://kohanaframework.org/guide/about.install#modules
+ */
+$modules = 'modules';
 
+/**
+ * The directory in which the Kohana resources are located. The system
+ * directory must contain the classes/kohana.php file.
+ *
+ * @link http://kohanaframework.org/guide/about.install#system
+ */
+$system = 'system';
 
-?>
+/**
+ * The default extension of resource files. If you change this, all resources
+ * must be renamed to use the new extension.
+ *
+ * @link http://kohanaframework.org/guide/about.install#ext
+ */
+define('EXT', '.php');
 
-<!-- HTML CODE with Embeded PHP-->
-<div>
-    <?php
-    /*
-     * If login url is there then display login button
-     * else print the retieved data
-    */
-    if (isset($authUrl)) {
-        echo "<a class='login' href='" . $authUrl . "'><img src='gplus-lib/signin_button.png' height='50px'/></a>";
-    } else {
-        print "ID: {$id} <br>";
-        print "Name of user: {$name} <br>";
-        print "Email: {$email } <br>";
-        print "Image : {$profile_image_url} <br>";
-        print "Cover  :{$cover_image_url} <br>";
-        print "Url: {$profile_url} <br><br>";
-        echo "<a class='logout' href='?logout'><button>Logout</button></a>";
-    }
-    ?>
-</div>
+/**
+ * Set the PHP error reporting level. If you set this in php.ini, you remove this.
+ * @link http://www.php.net/manual/errorfunc.configuration#ini.error-reporting
+ *
+ * When developing your application, it is highly recommended to enable notices
+ * and strict warnings. Enable them by using: E_ALL | E_STRICT
+ *
+ * In a production environment, it is safe to ignore notices and strict warnings.
+ * Disable them by using: E_ALL ^ E_NOTICE
+ *
+ * When using a legacy application with PHP >= 5.3, it is recommended to disable
+ * deprecated notices. Disable with: E_ALL & ~E_DEPRECATED
+ */
+error_reporting(E_ALL | E_STRICT);
+
+/**
+ * End of standard configuration! Changing any of the code below should only be
+ * attempted by those with a working knowledge of Kohana internals.
+ *
+ * @link http://kohanaframework.org/guide/using.configuration
+ */
+
+// Set the full path to the docroot
+define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
+
+// Make the application relative to the docroot, for symlink'd index.php
+if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
+	$application = DOCROOT.$application;
+
+// Make the modules relative to the docroot, for symlink'd index.php
+if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
+	$modules = DOCROOT.$modules;
+
+// Make the system relative to the docroot, for symlink'd index.php
+if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
+	$system = DOCROOT.$system;
+
+// Define the absolute paths for configured directories
+define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
+define('MODPATH', realpath($modules).DIRECTORY_SEPARATOR);
+define('SYSPATH', realpath($system).DIRECTORY_SEPARATOR);
+
+// Clean up the configuration vars
+unset($application, $modules, $system);
+
+if (file_exists('install'.EXT))
+{
+	// Load the installation check
+	return include 'install'.EXT;
+}
+
+/**
+ * Define the start time of the application, used for profiling.
+ */
+if ( ! defined('KOHANA_START_TIME'))
+{
+	define('KOHANA_START_TIME', microtime(TRUE));
+}
+
+/**
+ * Define the memory usage at the start of the application, used for profiling.
+ */
+if ( ! defined('KOHANA_START_MEMORY'))
+{
+	define('KOHANA_START_MEMORY', memory_get_usage());
+}
+
+// Bootstrap the application
+require APPPATH.'bootstrap'.EXT;
+
+if (PHP_SAPI == 'cli') // Try and load minion
+{
+	class_exists('Minion_Task') OR die('Please enable the Minion module for CLI support.');
+	set_exception_handler(array('Minion_Exception', 'handler'));
+
+	Minion_Task::factory(Minion_CLI::options())->execute();
+}
+else
+{
+	/**
+	 * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
+	 * If no source is specified, the URI will be automatically detected.
+	 */
+	echo Request::factory(TRUE, array(), FALSE)
+		->execute()
+		->send_headers(TRUE)
+		->body();
+}
