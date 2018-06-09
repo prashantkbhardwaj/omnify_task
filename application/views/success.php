44936be07e9dfd010ab3febc79fad666a4f2e33e
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-06-09 22:48:16 +0530

    fix local

--- application/views/success.php
+++ application/views/success.php
@@ -1,19 +1,42 @@
 <?php
-if (isset($_GET['name'])) {
-  echo "<h1>Success</h1><br>";
-  $name = $_GET['name'];
-  $results = json_decode($_GET['results']);
+if (isset($_GET['token'])) {
+	$_SESSION['access_token'] = $_GET['token'];
+  $client->setAccessToken($_SESSION['access_token']);
+  $me = $plus->people->get('me');
 
-  if (empty($results->getItems())) {
-      print "No upcoming events found.\n";
-  } else {
-      print "Upcoming events:\n";
-      foreach ($results->getItems() as $event) {
-          $start = $event->start->dateTime;
-          if (empty($start)) {
-              $start = $event->start->date;
-          }
-          printf("%s (%s)\n", $event->getSummary(), $start);
-      }
-  }
+  // Get User data
+  $id = $me['id'];
+  $name =  $me['displayName'];
+  $email =  $me['emails'][0]['value'];
+  $profile_image_url = $me['image']['url'];
+  $cover_image_url = $me['cover']['coverPhoto']['url'];
+  $profile_url = $me['url'];
+
+} else {
+  // get the login url   
+  $authUrl = $client->createAuthUrl();
 }
+
+
+?>
+
+<!-- HTML CODE with Embeded PHP-->
+<div>
+    <?php
+    /*
+     * If login url is there then display login button
+     * else print the retieved data
+    */
+    if (isset($authUrl)) {
+        echo "<a class='login' href='" . $authUrl . "'><img src='gplus-lib/signin_button.png' height='50px'/></a>";
+    } else {
+        print "ID: {$id} <br>";
+        print "Name: {$name} <br>";
+        print "Email: {$email } <br>";
+        print "Image : {$profile_image_url} <br>";
+        print "Cover  :{$cover_image_url} <br>";
+        print "Url: {$profile_url} <br><br>";
+        echo "<a class='logout' href='?logout'><button>Logout</button></a>";
+    }
+    ?>
+</div>
