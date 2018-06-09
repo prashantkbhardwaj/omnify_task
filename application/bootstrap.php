d53f9573a41da865859aba44a28d43f44e5ccaa0
Ubuntu <ubuntu@ip-172-31-17-20.ap-south-1.compute.internal>
2018-06-09 10:59:19 +0000

    fix ser

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -144,6 +144,12 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
+Route::set('testing', '(<controller>(/<action>(/<id>)))')
+        ->defaults(array(
+                'controller' => 'testing',
+                'action'     => 'index',
+        ));
+
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'welcome',
