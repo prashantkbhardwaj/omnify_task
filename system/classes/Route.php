bdda0ecbf6e4cc24cd4fa12615c8e8d636c10065
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-06-09 10:15:53 +0530

    fix from local

--- system/classes/Route.php
+++ system/classes/Route.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 class Route extends Kohana_Route {
-set('check', '(<controller>(/<action>(/<id>)))')
+public function set('check', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'check',
 		'action'     => 'index',
